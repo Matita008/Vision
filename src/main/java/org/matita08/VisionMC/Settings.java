0b4ed39af2ca6d2704c5bdfe47d2edc82d31d044
Matita008 <155455608+Matita008@users.noreply.github.com>
2024-11-02 11:30:28 +0100

    Create Settings.java

---
+++ src/main/java/org/matita08/VisionMC/Settings.java
@@ -0,0 +1,9 @@
+package org.matita08.VisionMC;
+
+public class Settings {
+    public static VisionBots bots;
+    public static VisionTicket ticket;
+    public static VisionStaff staff;
+    public static String ticketToken = "";
+    public static String staffToken = "";
+}
